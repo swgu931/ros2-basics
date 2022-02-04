624af0d142d02d30fbdc6055c49615e9965fa4c6
Nevertheless <swgu931@chol.com>
2022-02-04 14:57:33 +0000

    Add SBC (raspberry pi) setup shell script

---
+++ turtlebot3/sbc-setup.sh
@@ -0,0 +1,16 @@
+# SBC (Raspberry pi) setup for OpenCR setup
+## ref: https://emanual.robotis.com/docs/en/platform/turtlebot3/opencr_setup/#opencr-setup
+
+sudo dpkg --add-architecture armhf
+sudo apt update
+sudo apt install libc6:armhf
+
+export OPENCR_PORT=/dev/ttyACM0
+export OPENCR_MODEL=burger
+rm -rf ./opencr_update.tar.bz2
+
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+tar -xjf ./opencr_update.tar.bz2
+
+cd ~/opencr_update
+./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
